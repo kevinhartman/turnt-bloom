d21bb3cfc4fa3aaf207ff93bf2d89a9e26ae8406
cheinrich09 <cah2792@GDD-47.main.ad.rit.edu>
2014-04-14 17:32:05 -0400

    Finally Linked in Lua

--- Lua_Example/Lua_Example/Lua_Example.cpp
+++ Lua_Example/Lua_Example/Lua_Example.cpp
@@ -1,10 +1,16 @@
 // Lua_Example.cpp : Defines the entry point for the console application.
 //
-
-
+extern "C" {
+#include "lua.h"
+#include "lauxlib.h"
+#include "lualib.h"
+}
 
 int main(int argc, char* argv[])
 {
+	lua_State *L;
+	luaL_openlibs(L); 
+	lua_close(L);
 	return 0;
 }
 
